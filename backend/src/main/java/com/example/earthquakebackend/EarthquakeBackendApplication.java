aed5d04d2705048f10ef96705615a6cd07519593
DanevskiM <danevskimetodija5@gmail.com>
2026-04-15 23:36:28 +0200

    second commit

---
+++ backend/src/main/java/com/example/earthquakebackend/EarthquakeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.earthquakebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EarthquakeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EarthquakeBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/earthquakebackend/EarthquakeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.earthquakebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EarthquakeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
